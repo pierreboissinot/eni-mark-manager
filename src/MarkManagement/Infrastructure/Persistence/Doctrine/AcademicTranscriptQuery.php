f10fa6e1433ebed695d37b369b7bf7de790ead85
pierre <perso@pierreboissinot.me>
2018-05-04 10:16:20 +0200

    add computed data for each domain

--- src/MarkManagement/Infrastructure/Persistence/Doctrine/AcademicTranscriptQuery.php
+++ src/MarkManagement/Infrastructure/Persistence/Doctrine/AcademicTranscriptQuery.php
@@ -14,43 +14,56 @@ use Pb\MarkManagement\Domain\Student;
 
 final class AcademicTranscriptQuery
 {
-    /**
-     * @var EntityManagerInterface
-     */
-    private $entityManager;
+	/**
+	 * @var EntityManagerInterface
+	 */
+	private $entityManager;
 
-    public function __construct(EntityManagerInterface $entityManager)
-    {
-        $this->entityManager = $entityManager;
-    }
+	public function __construct(EntityManagerInterface $entityManager)
+	{
+		$this->entityManager = $entityManager;
+	}
 
-    public function find(string $identifier)
-    {
-            $queryBuilder = $this->entityManager->createQueryBuilder()
-		    ->select(
-			    sprintf(
-				    'NEW %s(mark.id, mark.value, mark.coefficient, subject.label, domain.label)',
-				    AcademicTranscript::class
-			    )
-		    )
-		    ->from(Domain::class, 'domain', 'domain.id')
-		    ->where('student.id = :identifier')
-		    ->setParameters([
-			    'identifier' => $identifier
-		    ])
-                    ->join('domain.subjects', 'subject')
-		    ->join('subject.marks', 'mark')
-		    ->leftJoin('mark.student', 'student')
-		    ->getQuery();
-	    $result = $queryBuilder->getResult();
-	    return $this->_group_by_domain($result);
-    }
+	public function find(string $identifier)
+	{
+		$queryBuilder = $this->entityManager->createQueryBuilder()
+			->select(
+				sprintf(
+					'NEW %s(mark.id, mark.value, mark.coefficient, subject.label, domain.label)',
+					AcademicTranscript::class
+				)
+			)
+			->from(Domain::class, 'domain', 'domain.id')
+			->where('student.id = :identifier')
+			->setParameters([
+				'identifier' => $identifier
+			])
+			->join('domain.subjects', 'subject')
+			->join('subject.marks', 'mark')
+			->leftJoin('mark.student', 'student')
+			->getQuery();
+		$result = $queryBuilder->getResult();
+		return $this->_group_by_domain($result);
+	}
 
-    function _group_by_domain($array) {
-    $return = array();
-    foreach($array as $val) {
-        $return[$val->getDomain()][] = $val;
-    }
-      return $return;
-    }
+	function _group_by_domain($array) {
+		$return = array();
+		foreach($array as $val) {
+			$return[$val->getDomain()]['marks'][] = $val;
+		}
+		foreach($return as $domain => $domainInfos) {
+			$return[$domain]['coefficientTotal'] = array_reduce($domainInfos['marks'], function($carry, $item) {
+				$carry += $item->getCoefficient();
+				return $carry;
+			}
+			);
+			$totalValue = array_reduce($domainInfos['marks'], function($carry, $item) {
+				$carry += $item->getValue();
+				return $carry;
+			});
+			$countValue = count($domainInfos['marks']);
+			$return[$domain]['average'] = $totalValue / $countValue;
+		}
+		return $return;
+	}
 }
